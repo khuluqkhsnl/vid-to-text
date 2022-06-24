34f9022df67a787ff67ce7c31f57e5c2068c62cf
Tony <tnybaskoro@gmail.com>
2022-06-24 22:18:12 +0700

    footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -10,12 +10,11 @@
             </div>
         </div>
         <div class="footer-bottom border-top pt-3 d-flex flex-column flex-sm-row align-items-center">
-            <a class="btn btn-primary text-white btn-rounded" href="https://themeforest.net/item/gull-bootstrap-laravel-admin-dashboard-template/23101970" target="_blank">Buy Gull HTML</a>
-                <span class="flex-grow-1"></span>
+            <span class="flex-grow-1"></span>
                 <div class="d-flex align-items-center">
                         <img class="logo" src="../../dist-assets/images/logo.png" alt="">
                         <div>
-                            <p class="m-0">&copy; 2018 Gull HTML</p>
+                            <p class="m-0">&copy; 2022 UCC IT TEAM</p>
                             <p class="m-0">All rights reserved</p>
                         </div>
                 </div>
